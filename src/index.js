b9b1b4d013f688c178fbb5b36b1821932c5847b1
Farukh Sajjad <farukhsajjad110@gmail.com>
2021-03-26 01:19:33 +0500

    updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { createElement } from 'react'
+import { renderToString } from 'react-dom/server'
+
+import ssrPrepass from 'react-ssr-prepass'
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const renderApp = async App => {
+  const element = createElement(App)
+  await ssrPrepass(element)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return renderToString(element)
+}
+
+ssrPrepass(<App />, (_element, instance) => {
+
+  if (instance !== undefined && typeof instance.fetchData === 'function') {
+    return instance.fetchData()
+  }
+})
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
